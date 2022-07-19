088093bc0ca0ada34ba8eb09ba1fb5173aaa87e4
Gustavo Valença <gustavovalenca15@gmail.com>
2022-07-19 15:53:22 -0300

    Aula 3

---
+++ Aula 3 - Console.log/index.js
@@ -0,0 +1,12 @@
+/*
+# Aspas e crases
+    console.log("'Hello' world");
+    console.log('Hello "world"');
+    console.log(`'Hello' "world"`);
+*/
+
+//console.log(13414, 1844444.384444, 'Gustavo');
+
+// Exercício
+console.log('Meu nome é "Gustavo". Estou aprendendo JavaScript às', 15, 'da tarde.');
+
